5c584772ecbd6bf38375d43739c06580296af769
Matt Clark <clarkm02@gmail.com>
2024-05-09 12:22:19 +0200

    feat: add product_ endpoint

--- node-back/src/server.ts
+++ node-back/src/server.ts
@@ -2,6 +2,8 @@ const express = require('express');
 import { type Request, type Response } from 'express';
 const cors = require('cors');
 const helmet = require('helmet');
+var path = require('path');
+var public_files = path.join(__dirname, 'public');
 
 import { routes } from './routes/routes'
 
@@ -14,6 +16,11 @@ server.use(express.json());
 server.get('/', (req: Request, res: Response) => {
     res.send('Welcome to the node app server!!!')
 });
+
+server.get('/product/:json', (req: Request, res: Response) => {
+  const { json } = req.params;
+  res.sendFile(path.join(public_files, `product_${json}.json`));
+})
 
 server.use('/api', routes)
 
